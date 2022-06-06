207cb1671843a88097ba94eda20b7024fcd80246
Oladiran Sunkanmi <zukodime1@gmail.com>
2022-06-06 16:31:08 -0700

    Positive or Negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,7 +11,7 @@
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX /2;
-	if (n>0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
@@ -19,7 +19,7 @@
 	{
 		printf("%d is zero\n", n);
 	}
-	if (n<0)
+	if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
