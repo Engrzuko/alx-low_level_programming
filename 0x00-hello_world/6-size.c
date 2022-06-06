05a20233ad1ec5dafe742e61c5f70242b6d49727
Oladiran Sunkanmi <zukodime1@gmail.com>
2022-06-06 10:27:39 -0700

    size type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	long long d;
 	float e;
 
-	printf("size of a char: %ld byte(s)\n"; sizeof (a));
-	printf("size of a int: %ld byte(s)\n"; sizeof (a));
-	printf("size of a long: %ld byte(s)\n"; sizeof (c));
-	printf("size of a long long: %ld byte(s)\n"; sizeof (d));
-	printf("size of a float: %ld byte(s)\n"; sizeof (e));
+	printf("size of a char: %ld byte(s)\n", sizeof (a));
+	printf("size of a int: %ld byte(s)\n", sizeof (a));
+	printf("size of a long: %ld byte(s)\n", sizeof (c));
+	printf("size of a long long: %ld byte(s)\n", sizeof (d));
+	printf("size of a float: %ld byte(s)\n", sizeof (e));
 	return (0);
 }
 
