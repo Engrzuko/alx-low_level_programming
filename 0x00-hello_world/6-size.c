262c25f6c1150ce23982bef3e7449ed38e2c5122
Oladiran Sunkanmi <zukodime1@gmail.com>
2022-06-06 10:24:26 -0700

    size type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * Main - Starting point
+ *
+ * Return 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long c;
+	long long d;
+	float e;
+
+	printf("size of a char: %ld byte(s)\n"; sizeof (a));
+	printf("size of a int: %ld byte(s)\n"; sizeof (a));
+	printf("size of a long: %ld byte(s)\n"; sizeof (c));
+	printf("size of a long long: %ld byte(s)\n"; sizeof (d));
+	printf("size of a float: %ld byte(s)\n"; sizeof (e));
+	return (0);
+}
+
