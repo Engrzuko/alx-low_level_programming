03ff3d151e7d1ab492e551f202be855f49c2a7e4
Oladiran Sunkanmi <zukodime1@gmail.com>
2022-06-06 20:18:37 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+	int num;
+	char lsb;
+
+	for (num = 0; num <= 16; num++)
+		putchar((num % 16) + '0');
+	for (lsb = 'a'; lsb <= 'f'; lsb++)
+		putchar(lsb);
+	putchar('\n');
+	return (0);
+}
