8d6de7d786fb067b4ee0e3f89aaccb1916dcf107
Oladiran Sunkanmi <zukodime1@gmail.com>
2022-06-06 20:03:40 -0700

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; alpha--)
+	putchar(alpha);
+	putchar('\n');
+	return (0);
+}
