53687c9d0d0e16045695649e77f4b6191c91976d
Oladiran Sunkanmi <zukodime1@gmail.com>
2022-06-06 17:32:35 -0700

    Print Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for(alpha = 'a'; alpha <= 'z'; alpha++);
+	putchar(alpha);
+	putchar('\n');
+	return (0);
+}
