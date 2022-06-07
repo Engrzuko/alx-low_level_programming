72dae9be85dc3841d2b92c9afc253b2694663cb5
Oladiran Sunkanmi <zukodime1@gmail.com>
2022-06-06 18:00:48 -0700

    Print 3 Alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+	char lc;
+	char Uc;
+
+	for ( lc = 'a'; lc <= 'z'; lc++)
+	putchar(lc);
+	for ( Uc = 'A'; Uc <= 'Z'; Uc++)
+	putchar(Uc);
+	putchar('\n');
+	return (0);
+}
+	
