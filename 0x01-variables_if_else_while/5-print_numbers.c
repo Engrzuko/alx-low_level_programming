b317435894ac8d14a3b809b208d67984690fdf61
Oladiran Sunkanmi <zukodime1@gmail.com>
2022-06-06 19:11:04 -0700

    Print numbers <10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio>
+#include <stdlib>
+#include <time.h>
+/**
+ * main - Writing numbers
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+	int n;
+
+	for (n = '0'; n < '10'; n++)
+	printf("n\n");
+	return (0);
+}
