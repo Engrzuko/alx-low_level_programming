43b4c011dd6e95eb11d1af1450cb001697b9bd4b
Oladiran Sunkanmi <zukodime1@gmail.com>
2022-06-06 18:58:02 -0700

    Print Alphabet -qe

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Writtinh alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ls;
+
+	for (ls = 'a'; ls <= 'z'; ls++)
+	{
+		if (lc != 'q' && lc != 'e')
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
