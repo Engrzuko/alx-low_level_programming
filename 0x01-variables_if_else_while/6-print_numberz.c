d9f012f209909d71233cfaf5ef9336687fc9be6f
Oladiran Sunkanmi <zukodime1@gmail.com>
2022-06-06 19:45:27 -0700

    Print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	putchar('n');
+	putchar('\n');
+	return (0);
+}
